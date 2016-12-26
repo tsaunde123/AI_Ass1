9b7db436410be0b8eb46f870b1e97ea3aad16334
Kacper Sokol <ks1591@my.bristol.ac.uk>
2016-12-26 15:06:08 +0000

    wp reformatted; duplicated predicates moved to oscar_library.pl; wp now requires oscar_lirary with parameter part(2) to work properly

--- ailp/library/wp.pl
+++ ailp/library/wp.pl
@@ -1,3 +1,22 @@
+/*
+ *  wp.pl
+ *  Wikipedia stuff
+ */
+
+:- module(wp,
+  [ agent_ask_oracle/4,  % Re-exported form oscar_library.pl
+    part/1,              % Re-exported form oscar_library.pl
+    wp/1,
+    wp/2,
+    actor/1,
+    link/1,
+    init_identity/0,
+    test/0
+  ]
+).
+
+% We need oscar_library.pl for agent_ask_oracle/4
+:- use_module(oscar_library).
 :- use_module(library(http/http_open)).
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
@@ -6,14 +25,15 @@
 :- use_module(library(http/json)).
 :- use_module(library(www_browser)).
 
+% Used to cache all Wikipedia pages fetched during one Prolog session
+:- dynamic wp_cache/2.
 
-%%% Wikipedia stuff
-:- dynamic wp_cache/2.  % used to cache all Wikipedia pages fetched during one Prolog session
-
-% wp(Q,WT) <- issue query Q to Wikipedia and return the page in wikitext format
-wp(Q,WT):-
+%%%%%%%%%% Wikipedia interaction %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Issue query Q to Wikipedia and return the page in wikitext format
+% wp(+Querry, -WikiText)
+wp(Q,WT) :-
   wp_cache(Q,WT),!.
-wp(Q,WT):-
+wp(Q,WT) :-
   wp_query2URL(Q,URL),
   http_open(URL, R, []),
   json_read(R, RR, []),
@@ -26,19 +46,21 @@ wp(Q,WT):-
   ),
   assert(wp_cache(Q,WT)).
 
-% wp(Q) <- issue query Q to Wikipedia and write the page in wikitext format to stdout
-wp(Q):-
+% Issue query Q to Wikipedia and write the page in wikitext format to stdout
+% wp(+Querry)
+wp(Q) :-
   wp(Q,WT),
   atomic_list_concat(L,'\n',WT),  % split text T into segments separated by newlines
   writelns(L).  % write the list of segments
 
-% wppd(Q,PD) <- issue query Q to Wikipedia and return the person's persondata
-wppd(Q,PD):-
+% Issue query Q to Wikipedia and return the person's persondata
+% wppd(+Querry, -PersonData)
+wppd(Q,PD) :-
   wp(Q,WT),
   wt_persondata(WT,PD).
 
 % assemble query Q into a URL to retrieve the page in JSON format
-wp_query2URL(Q,URL):-
+wp_query2URL(Q,URL) :-
   atomic_list_concat(QW,' ',Q),  % split query Q into words QW
   atomic_list_concat(QW,'%20',QQ),  % reassemble query QQ with '%20' between words from QW
   atomic_list_concat([
@@ -47,11 +69,9 @@ wp_query2URL(Q,URL):-
     '&prop=revisions&rvprop=content&rawcontinue'
     ],URL).
 
-
-%%% Wikitext stuff
-
+%%%%%%%%%% JSON operations %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % decompose JSON Prolog term T until wikitext component is located
-wt_get(J,Text):-
+wt_get(J,Text) :-
   ( J = ('*'=Text) -> true
   ; J = (json(L)) -> wt_get(L,Text)
   ; J = (_=json(L)) -> wt_get(L,Text)
@@ -60,38 +80,38 @@ wt_get(J,Text):-
   ).
 
 % find bracketed elements; only works if unnested
-wt_element(WT,Begin,End,Element):-
+wt_element(WT,Begin,End,Element) :-
   atomic_list_concat1(Ls,Begin,WT),
   member(X,Ls),
   atomic_list_concat1([Element|_],End,X),
   Element \= ''.
 
-wt_link(WT,Link):-
+wt_link(WT,Link) :-
   wt_link(WT,Link,_Anchor,_WT_Link).
 
-wt_link(WT,Link,Anchor,WT_Link):-
+wt_link(WT,Link,Anchor,WT_Link) :-
   wt_element(WT,'[[',']]',Link0),
   ( atomic_list_concat1([Link,Anchor],'|',Link0) -> true
   ; otherwise -> Link=Link0, Anchor=Link0
   ),
   atomic_list_concat(['[[',Link0,']]'],WT_Link).
 
-wt_template(WT,Template,WT_Template):-
+wt_template(WT,Template,WT_Template) :-
   wt_element(WT,'{{','}}',Template),
   atomic_list_concat(['{{',Template,'}}'],WT_Template).
 
-wt_ref(WT,Ref,WT_Ref):-
+wt_ref(WT,Ref,WT_Ref) :-
   wt_element(WT,'<ref>','</ref>',Ref),
   atomic_list_concat(['<ref>',Ref,'</ref>'],WT_Ref).
 
-wt_persondata(WT,PD):-
+wt_persondata(WT,PD) :-
   wt_template(WT,Template,_WT_Template),
   ( atomic_list_concat(['',PD0],'Persondata',Template) -> atomic_list_concat(PD1,'|',PD0)
   ; atomic_list_concat(['',_,PD0],'Persondata',Template) -> atomic_list_concat(PD1,'|',PD0)
   ),get_persondata(PD1,PD).
 
 get_persondata([],[]).
-get_persondata([X|Xs],Out):-
+get_persondata([X|Xs],Out) :-
   ( atomic_list_concat1(L,=,X) ->
     ( L = [_A,'\n'] -> Out = Ys
     ; L = [_A,' ']  -> Out = Ys
@@ -101,26 +121,24 @@ get_persondata([X|Xs],Out):-
   ),
   get_persondata(Xs,Ys).
 
-
-%%% Utilities
-
+%%%%%%%%%% Utilities %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % write a list of items with a newline after each
-writelns([]):-nl.
-writelns([H|T]):-
+writelns([]) :- nl.
+writelns([H|T]) :-
   write(H),nl,
   writelns(T).
 
 % version of atomic_list_concat/3 that fails if separator doesn't occur
-atomic_list_concat1(L, S, A):-
+atomic_list_concat1(L, S, A) :-
   atomic_list_concatN(N, L, S, A),
   N>0.
 
-atomic_list_concatN(N, L, S, A):-
+atomic_list_concatN(N, L, S, A) :-
   atomic_list_concat(L, S, A),
   length(L,N0), N is N0-1.
 
 
-%%% Actors and links
+%%%%%%%%%% Actors and links %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 actor('Billy Bob Thornton').
 actor('Frances McDormand').
 actor('Gabriel Byrne').
@@ -153,29 +171,20 @@ link('The Big Lebowski').
 link('Tony Award').
 link('Los Angeles').
 
-random_actor(A):-
+random_actor(A) :-
   findall(A,actor(A),L),
   random_member(A,L).
 
-random_link(A,L):-
+random_link(A,L) :-
   actor(A),
   findall(L,(link(L),wp(A,WT),wt_link(WT,L)),Ls),
   random_member(L,Ls).
 
-
-%%% Your solution goes here
-
-% find_identity(-A) <- find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
-find_identity(A):-
-  A='Not yet implemented'.
-
-
-%%% Testing
-
+%%%%%%%%%% Testing %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 :- dynamic ailp_identity/1.
 
 % asserts a random actor identity
-init_identity:-
+init_identity :-
   random_actor(A),
   init_identity(A).
 
@@ -185,35 +194,14 @@ init_identity(A) :-
   retractall(ailp_identity(_)),
   assert(ailp_identity(A)).
 
-:-init_identity.
+:- init_identity.
 
 % failure-driven loop to test all identities
-test:-
+test :-
   init_identity(_A),  % initialise; don't reveal identity
-  find_identity(A),  % program under test
+  user:find_identity(A),  % program under test
   ailp_identity(I),
   ( A=I -> writeln('I am':A)
   ; otherwise -> writeln('Wrong answer':A)
   ),fail.
 test.
-
-%% repeated from oscar_library.pl for testing purposes %%
-
-% agent_ask_oracle(+Agent, +OID, +Question, -Answer)
-% Agent's position needs to be map_adjacent to oracle identified by OID
-%% Test query to be used: :-agent_ask_oracle(oscar,o(1),link,L). %%
-agent_ask_oracle(Agent, OID, Question, Answer) :-
-  nonvar(Agent),
-  nonvar(OID),
-  nonvar(Question),
-  var(Answer),
-  %agent_current_position(Agent,Pos),  % ignore agent position for testing
-  %map_adjacent(Pos, AdjPos, OID),  % ignore agent position for testing
-  OID = o(_),
-  internal_object(OID, _AdjPos, Options),
-  member(question(Q)/answer(A),Options),
-  ( Question=Q -> Answer=A ; Answer='I do not know' ).
-
-internal_object(o(1),p(5,3),[question(link)/answer(Link)]):-
-  ailp_identity(A),
-  random_link(A,Link).
