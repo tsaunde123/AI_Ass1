761b25661f22cca626bdc4b28cf9a6bf3e898579
Kacper Sokol <ks1591@my.bristol.ac.uk>
2016-12-26 13:29:54 +0000

    Reformatted: tabs into spaces

--- ailp/library/wp.pl
+++ ailp/library/wp.pl
@@ -8,99 +8,98 @@
 
 
 %%% Wikipedia stuff
-
-:- dynamic wp_cache/2.	% used to cache all Wikipedia pages fetched during one Prolog session
+:- dynamic wp_cache/2.  % used to cache all Wikipedia pages fetched during one Prolog session
 
 % wp(Q,WT) <- issue query Q to Wikipedia and return the page in wikitext format
 wp(Q,WT):-
-	wp_cache(Q,WT),!.
+  wp_cache(Q,WT),!.
 wp(Q,WT):-
-	wp_query2URL(Q,URL),
+  wp_query2URL(Q,URL),
   http_open(URL, R, []),
   json_read(R, RR, []),
   close(R),
-	%http_get(URL,R,[]),
-	%atom_json_term(R,RR,[]),
-	wt_get(RR,WT0),
-	( atomic_list_concat1(_L,'#REDIRECT',WT0) -> wt_link(WT0,QQ),wp(QQ,WT)
-	; otherwise -> WT=WT0
-	),
-	assert(wp_cache(Q,WT)).
+  %http_get(URL,R,[]),
+  %atom_json_term(R,RR,[]),
+  wt_get(RR,WT0),
+  ( atomic_list_concat1(_L,'#REDIRECT',WT0) -> wt_link(WT0,QQ),wp(QQ,WT)
+  ; otherwise -> WT=WT0
+  ),
+  assert(wp_cache(Q,WT)).
 
 % wp(Q) <- issue query Q to Wikipedia and write the page in wikitext format to stdout
 wp(Q):-
-	wp(Q,WT),
-	atomic_list_concat(L,'\n',WT),	% split text T into segments separated by newlines
-	writelns(L).	% write the list of segments
+  wp(Q,WT),
+  atomic_list_concat(L,'\n',WT),  % split text T into segments separated by newlines
+  writelns(L).  % write the list of segments
 
 % wppd(Q,PD) <- issue query Q to Wikipedia and return the person's persondata
 wppd(Q,PD):-
-	wp(Q,WT),
-	wt_persondata(WT,PD).
+  wp(Q,WT),
+  wt_persondata(WT,PD).
 
 % assemble query Q into a URL to retrieve the page in JSON format
 wp_query2URL(Q,URL):-
-	atomic_list_concat(QW,' ',Q),	% split query Q into words QW
-	atomic_list_concat(QW,'%20',QQ),	% reassemble query QQ with '%20' between words from QW
-	atomic_list_concat([
-		'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=',
-		QQ,
-		'&prop=revisions&rvprop=content&rawcontinue'
-		],URL).
+  atomic_list_concat(QW,' ',Q),  % split query Q into words QW
+  atomic_list_concat(QW,'%20',QQ),  % reassemble query QQ with '%20' between words from QW
+  atomic_list_concat([
+    'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=',
+    QQ,
+    '&prop=revisions&rvprop=content&rawcontinue'
+    ],URL).
 
 
 %%% Wikitext stuff
 
 % decompose JSON Prolog term T until wikitext component is located
 wt_get(J,Text):-
-	( J = ('*'=Text) -> true
-	; J = (json(L)) -> wt_get(L,Text)
-	; J = (_=json(L)) -> wt_get(L,Text)
-	; J = (_=L) -> wt_get(L,Text)
-	; J = [H|T] -> ( wt_get(H,Text) ; wt_get(T,Text) )
-	).
+  ( J = ('*'=Text) -> true
+  ; J = (json(L)) -> wt_get(L,Text)
+  ; J = (_=json(L)) -> wt_get(L,Text)
+  ; J = (_=L) -> wt_get(L,Text)
+  ; J = [H|T] -> ( wt_get(H,Text) ; wt_get(T,Text) )
+  ).
 
 % find bracketed elements; only works if unnested
 wt_element(WT,Begin,End,Element):-
-	atomic_list_concat1(Ls,Begin,WT),
-	member(X,Ls),
-	atomic_list_concat1([Element|_],End,X),
-	Element \= ''.
+  atomic_list_concat1(Ls,Begin,WT),
+  member(X,Ls),
+  atomic_list_concat1([Element|_],End,X),
+  Element \= ''.
 
 wt_link(WT,Link):-
-	wt_link(WT,Link,_Anchor,_WT_Link).
+  wt_link(WT,Link,_Anchor,_WT_Link).
 
 wt_link(WT,Link,Anchor,WT_Link):-
-	wt_element(WT,'[[',']]',Link0),
-	( atomic_list_concat1([Link,Anchor],'|',Link0) -> true
-	; otherwise -> Link=Link0, Anchor=Link0
-	),
-	atomic_list_concat(['[[',Link0,']]'],WT_Link).
+  wt_element(WT,'[[',']]',Link0),
+  ( atomic_list_concat1([Link,Anchor],'|',Link0) -> true
+  ; otherwise -> Link=Link0, Anchor=Link0
+  ),
+  atomic_list_concat(['[[',Link0,']]'],WT_Link).
 
 wt_template(WT,Template,WT_Template):-
-	wt_element(WT,'{{','}}',Template),
-	atomic_list_concat(['{{',Template,'}}'],WT_Template).
+  wt_element(WT,'{{','}}',Template),
+  atomic_list_concat(['{{',Template,'}}'],WT_Template).
 
 wt_ref(WT,Ref,WT_Ref):-
-	wt_element(WT,'<ref>','</ref>',Ref),
-	atomic_list_concat(['<ref>',Ref,'</ref>'],WT_Ref).
+  wt_element(WT,'<ref>','</ref>',Ref),
+  atomic_list_concat(['<ref>',Ref,'</ref>'],WT_Ref).
 
 wt_persondata(WT,PD):-
-	wt_template(WT,Template,_WT_Template),
-	( atomic_list_concat(['',PD0],'Persondata',Template) -> atomic_list_concat(PD1,'|',PD0)
-	; atomic_list_concat(['',_,PD0],'Persondata',Template) -> atomic_list_concat(PD1,'|',PD0)
-	),get_persondata(PD1,PD).
+  wt_template(WT,Template,_WT_Template),
+  ( atomic_list_concat(['',PD0],'Persondata',Template) -> atomic_list_concat(PD1,'|',PD0)
+  ; atomic_list_concat(['',_,PD0],'Persondata',Template) -> atomic_list_concat(PD1,'|',PD0)
+  ),get_persondata(PD1,PD).
 
 get_persondata([],[]).
 get_persondata([X|Xs],Out):-
-	( atomic_list_concat1(L,=,X) ->
-		( L = [_A,'\n'] -> Out = Ys
-		; L = [_A,' ']  -> Out = Ys
-		; L = [A,B]     -> Out = [A=B|Ys]
-		)
-	; otherwise -> Out = Ys	% skip X if it doesn't contain =
-	),
-	get_persondata(Xs,Ys).
+  ( atomic_list_concat1(L,=,X) ->
+    ( L = [_A,'\n'] -> Out = Ys
+    ; L = [_A,' ']  -> Out = Ys
+    ; L = [A,B]     -> Out = [A=B|Ys]
+    )
+  ; otherwise -> Out = Ys  % skip X if it doesn't contain =
+  ),
+  get_persondata(Xs,Ys).
 
 
 %%% Utilities
@@ -108,21 +107,20 @@ get_persondata([X|Xs],Out):-
 % write a list of items with a newline after each
 writelns([]):-nl.
 writelns([H|T]):-
-	write(H),nl,
-	writelns(T).
+  write(H),nl,
+  writelns(T).
 
 % version of atomic_list_concat/3 that fails if separator doesn't occur
 atomic_list_concat1(L, S, A):-
-	atomic_list_concatN(N, L, S, A),
-	N>0.
+  atomic_list_concatN(N, L, S, A),
+  N>0.
 
 atomic_list_concatN(N, L, S, A):-
-	atomic_list_concat(L, S, A),
-	length(L,N0), N is N0-1.
+  atomic_list_concat(L, S, A),
+  length(L,N0), N is N0-1.
 
 
 %%% Actors and links
-
 actor('Billy Bob Thornton').
 actor('Frances McDormand').
 actor('Gabriel Byrne').
@@ -156,20 +154,20 @@ link('Tony Award').
 link('Los Angeles').
 
 random_actor(A):-
-	findall(A,actor(A),L),
-	random_member(A,L).
+  findall(A,actor(A),L),
+  random_member(A,L).
 
 random_link(A,L):-
-	actor(A),
-	findall(L,(link(L),wp(A,WT),wt_link(WT,L)),Ls),
-	random_member(L,Ls).
+  actor(A),
+  findall(L,(link(L),wp(A,WT),wt_link(WT,L)),Ls),
+  random_member(L,Ls).
 
 
 %%% Your solution goes here
 
 % find_identity(-A) <- find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
 find_identity(A):-
-	A='Not yet implemented'.
+  A='Not yet implemented'.
 
 
 %%% Testing
@@ -178,25 +176,25 @@ find_identity(A):-
 
 % asserts a random actor identity
 init_identity:-
-	random_actor(A),
-	init_identity(A).
+  random_actor(A),
+  init_identity(A).
 
 % can be used to backtrack over all actor identities
 init_identity(A) :-
-	actor(A),
-	retractall(ailp_identity(_)),
-	assert(ailp_identity(A)).
+  actor(A),
+  retractall(ailp_identity(_)),
+  assert(ailp_identity(A)).
 
 :-init_identity.
 
 % failure-driven loop to test all identities
 test:-
-	init_identity(_A),	% initialise; don't reveal identity
-	find_identity(A),	% program under test
-	ailp_identity(I),
-	( A=I -> writeln('I am':A)
-	; otherwise -> writeln('Wrong answer':A)
-	),fail.
+  init_identity(_A),  % initialise; don't reveal identity
+  find_identity(A),  % program under test
+  ailp_identity(I),
+  ( A=I -> writeln('I am':A)
+  ; otherwise -> writeln('Wrong answer':A)
+  ),fail.
 test.
 
 %% repeated from oscar_library.pl for testing purposes %%
@@ -205,17 +203,17 @@ test.
 % Agent's position needs to be map_adjacent to oracle identified by OID
 %% Test query to be used: :-agent_ask_oracle(oscar,o(1),link,L). %%
 agent_ask_oracle(Agent, OID, Question, Answer) :-
-	nonvar(Agent),
-	nonvar(OID),
-	nonvar(Question),
-	var(Answer),
-	%agent_current_position(Agent,Pos),	% ignore agent position for testing
-	%map_adjacent(Pos, AdjPos, OID),	% ignore agent position for testing
-	OID = o(_),
-	internal_object(OID, _AdjPos, Options),
-	member(question(Q)/answer(A),Options),
-	( Question=Q -> Answer=A ; Answer='I do not know' ).
+  nonvar(Agent),
+  nonvar(OID),
+  nonvar(Question),
+  var(Answer),
+  %agent_current_position(Agent,Pos),  % ignore agent position for testing
+  %map_adjacent(Pos, AdjPos, OID),  % ignore agent position for testing
+  OID = o(_),
+  internal_object(OID, _AdjPos, Options),
+  member(question(Q)/answer(A),Options),
+  ( Question=Q -> Answer=A ; Answer='I do not know' ).
 
 internal_object(o(1),p(5,3),[question(link)/answer(Link)]):-
-	ailp_identity(A),
-	random_link(A,Link).
+  ailp_identity(A),
+  random_link(A,Link).
