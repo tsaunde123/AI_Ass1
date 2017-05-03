72f219d21a8ccd3b1a55616e829ff8dbc952ec17
Kacper Sokol <ks1591@bristol.ac.uk>
2017-05-03 14:51:12 +0100

    Clear indication of necessary predicates

--- assignment2_wp_12345.pl
+++ assignment2_wp_12345.pl
@@ -1,4 +1,18 @@
+% candidate_number(12345).
+
 % Find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
 % find_identity(-A)
 find_identity(A):-
+  ( part(2) -> find_identity_2(A)
+  ; part(3) -> find_identity_3(A)
+  ; part(4) -> find_identity_4(A)
+  ).
+
+find_identity_2(A):-
+  A='Not yet implemented'.
+
+find_identity_3(A):-
+  A='Not yet implemented'.
+
+find_identity_4(A):-
   A='Not yet implemented'.
